4e5780173046eb63e122291b4a89dd86bbb20fd6
Theeohn テオン <theeohn@gmail.com>
2026-08-17 16:04:12 -0500

    Create app.js

---
+++ assets/app.js
@@ -0,0 +1,35 @@
+// =============================================================================
+//  Name: Vault-Tec Timer
+//  Authors: @Theeohn, @trekker87
+//  License: MIT
+//  Repository: https://github.com/Theeohn/Vault-Tec-Timer-3000a
+// =============================================================================
+
+(function () {
+  let screen;
+
+  function goTo(file) {
+    if (screen && screen.remove) screen.remove();
+    screen = null;
+    h.reset();
+    try {
+      screen = eval(require('fs').readFileSync(file))(goTo);
+    } catch (e) {
+      Pip.log('VAULTTIMER failed to load ' + file + ': ' + e);
+      if (file !== 'HOLO/VAULT_TEC_TIMER/TITLE.JS') {
+        screen = eval(require('fs').readFileSync('HOLO/VAULT_TEC_TIMER/TITLE.JS'))(goTo);
+      }
+    }
+  }
+
+  goTo('HOLO/VAULT_TEC_TIMER/TITLE.JS');
+
+  return {
+    id: 'VAULTTIMER',
+    notDefault: true,
+    fullscreen: true,
+    remove: function () {
+      if (screen && screen.remove) screen.remove();
+    },
+  };
+});
